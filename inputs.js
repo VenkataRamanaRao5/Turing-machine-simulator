9b397ae1ded52a9ae0098d6ae0120a739cf1efba
VenkataRamanaRao5 <venkatraocv2005@gmail.com>
2024-05-27 20:15:56 +0530

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -60,7 +60,7 @@ for (let i = 0; i < sets.length; i++) {
                 state = table.rows.item(1).cells[0].textContent
                 for (let i = 1; i < table.rows.length; i++) {
                     let row = table.rows.item(i).cells
-                    abbreviatedTable.set(JSON.stringify([row[0].textContent, row[1].textContent]), [row[3].textContent, row[2].textContent.split(/, ?/).filter(e => e)])
+                    abbreviatedTable.set(JSON.stringify([row[0].textContent, row[1].textContent]), [row[3].textContent, row[2].textContent.split(/, ?/).filter(e => e).map(a => (a == 'B' || a == ' ') ? '' : a)])
                 }
                 if (vars.rows.length > 1) {
                     for (let i = 1; i < vars.rows.length; i++) {
